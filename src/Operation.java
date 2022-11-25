b909aa2f1b4aee2c62aadd0c6881c6bc5fff5198
sooni2 <jinong22@gmail.com>
2022-11-25 20:50:30 +0900

    스프링 입문 주차 SA 과제

---
+++ src/Operation.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class busOperation{
+
+    busOperation() {}; //기본 생성자
+
+    Bus bus1 = new Bus();
+
+    Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+
+
+
+}
