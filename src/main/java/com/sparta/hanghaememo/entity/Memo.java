dead000bd6f523c34e17e3567e75f6b80f66738e
sooni2 <jinong22@gmail.com>
2022-11-28 17:44:43 +0900

    Spring 1주차 과제 template

---
+++ src/main/java/com/sparta/hanghaememo/entity/Memo.java
@@ -0,0 +1,33 @@
+package com.sparta.hanghaememo.entity;
+
+import com.sparta.hanghaememo.dto.MemoRequestDto;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Memo extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String contents;
+
+    public Memo(MemoRequestDto requestDto){
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+    }
+
+    public void update(MemoRequestDto requestDto) {
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+    }
+}
