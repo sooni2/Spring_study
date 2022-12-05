c52eca9a4a01b4d5479748c2b7aa803df1f5a692
sooni2 <jinong22@gmail.com>
2022-12-05 16:08:50 +0900

    feat - 게시글 조회 기능 추가

--- src/main/java/com/sparta/hanghaememo/repository/MemoRepository.java
+++ src/main/java/com/sparta/hanghaememo/repository/MemoRepository.java
@@ -5,8 +5,13 @@ import com.sparta.hanghaememo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
     List<Memo> findAllByOrderByModifiedAtDesc();
+
+    List<Memo> findAllByUserId(Long userId);
+
+    Optional<Memo> findByIdAndUserId(Long id, Long userId);
 
 }
