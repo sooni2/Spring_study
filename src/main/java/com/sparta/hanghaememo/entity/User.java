1b6e759d54792bd57c3c7ea3257d924e0dbeb7e3
sooni2 <jinong22@gmail.com>
2022-12-06 16:02:38 +0900

    feat - 아이디, 비밀번호 입력 제한 기능

--- src/main/java/com/sparta/hanghaememo/entity/User.java
+++ src/main/java/com/sparta/hanghaememo/entity/User.java
@@ -1,5 +1,7 @@
 package com.sparta.hanghaememo.entity;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,9 +19,13 @@ public class User {
     // nullable: null 허용 여부
     // unique: 중복 허용 여부 (false 일때 중복 허용)
     @Column(nullable = false, unique = true)
+    @Size(min = 4, max = 10, message = "아이디의 길이는 4자에서 10자 사이입니다")
+    @Pattern(regexp = "[a-z0-9]*$", message = "아이디 형식이 일치하지 않습니다")
     private String username;
 
     @Column(nullable = false)
+    @Size(min = 8, max = 15, message = "비밀번호의 길이는 8자에서 15자 사이입니다")
+    @Pattern(regexp = "[a-zA-Z0-9]*$", message = "비밀번호 형식이 일치하지 않습니다")
     private String password;
 
     @Column(nullable = false)
