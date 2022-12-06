34f241f073fda3b62d5fa56a687621eba6dff99a
sooni2 <jinong22@gmail.com>
2022-12-06 14:43:30 +0900

    fix - 새로운 과제에 맞게 기존 변수 수정

--- src/main/java/com/sparta/hanghaememo/dto/MemoRequestDto.java
+++ src/main/java/com/sparta/hanghaememo/dto/MemoRequestDto.java
@@ -2,13 +2,19 @@ package com.sparta.hanghaememo.dto;
 
 import com.sparta.hanghaememo.entity.Memo;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class MemoRequestDto {
 
     private String contents;
-    private String password;
     private String title;
-    private String author;
-    private String userId;
+    private Long id;
+
+    public MemoRequestDto(Memo memo) {
+        this.contents = memo.getContents();
+        this.title = memo.getTitle();
+        this.id = memo.getId();
+    }
 }
