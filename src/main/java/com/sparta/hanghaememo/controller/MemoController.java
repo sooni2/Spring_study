f196e20c2a7b96d5fc91a905b1408f213ba7f025
sooni2 <jinong22@gmail.com>
2022-12-06 14:44:57 +0900

    fix - MemoController 수정
    - 헤더를 포함해서 request, response 받아오는 값 수정
    - 비밀번호 확인 부분 삭제

--- src/main/java/com/sparta/hanghaememo/controller/MemoController.java
+++ src/main/java/com/sparta/hanghaememo/controller/MemoController.java
@@ -1,8 +1,8 @@
 package com.sparta.hanghaememo.controller;
 
 import com.sparta.hanghaememo.dto.MemoRequestDto;
+import com.sparta.hanghaememo.dto.MemoResponseDto;
 import com.sparta.hanghaememo.entity.Memo;
-import com.sparta.hanghaememo.repository.MemoRepository;
 import com.sparta.hanghaememo.service.MemoService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +16,6 @@ import java.util.List;
 public class MemoController {
 
     private final MemoService memoService;
-    private final MemoRepository memoRepository;
 
     @GetMapping("/")
     public ModelAndView home() {
@@ -25,32 +24,28 @@ public class MemoController {
 
     //게시글 작성하기
     @PostMapping("/api/memos")
-    public Memo createMemo(@RequestBody MemoRequestDto requestDto, HttpServletRequest request) {
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto, HttpServletRequest request) {
         return memoService.createMemo(requestDto, request);
     }
 
-    //게시글 조회하기
+    //게시글 전체 조회하기
     @GetMapping("/api/memos")
     public List<MemoRequestDto> getMemos(HttpServletRequest request) {
 
         return memoService.getMemos(request);
     }
 
-    @DeleteMapping("/api/memos/{id}")
-    public Long deleteMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
-        Memo memo = getMemo(id);
-        if (memo.getPassword().equals(requestDto.getPassword())) {
-            memoService.deleteMemo(id);
-            return id;
-        } else return 0L;
+    //게시글 수정하기
+    @PutMapping("/api/memos/{id}")
+    public Memo updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto, HttpServletRequest request) {
+        return memoService.update(id, requestDto, request);
     }
 
-    //중복 메소드 묶기
-    private Memo getMemo(Long id) {
-        Memo memo = memoRepository.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("아이디가 존재하지 않습니다.")
-        );
-        return memo;
+    //게시글 삭제하기
+    @DeleteMapping("/api/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id, HttpServletRequest request) {
+        return memoService.deleteMemo(id, request);
     }
+
 }
 
